6aa5c918e137084c93cf5fce3e182041465ebd0e
TurkishCat <s129096@ap.be>
2023-05-23 11:19:41 +0200

    Delete routes.dart

--- parkpal/lib/routes/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import '../main.dart';
-import '../login/login_screen.dart';
-
-// final Map<String, WidgetBuilder> routes = {
-//   '/home': (BuildContext context) => const MapApp(),
-//   '/': (BuildContext context) =>  LoginPage(),
-// };
